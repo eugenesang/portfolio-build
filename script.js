db1038010b3d55ea245aa87f818d7f89a83fb464
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-12 13:17:55 +0300

    import secrets

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
+import { firebaseConfig } from "./secrets.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
