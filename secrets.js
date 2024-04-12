e3f3685f6c1c3a6faea6e10fc125773529995673
Eugene Kiprop Sang <eugenekipropsangtg@gmail.com>
2024-04-12 13:20:38 +0300

    the secret is publicly visible anyway

---
+++ secrets.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDP8EPzCgM-qE41AgarRM7hNy3Gt8jgCVU",
+    authDomain: "eugene-sang.firebaseapp.com",
+    projectId: "eugene-sang",
+    storageBucket: "eugene-sang.appspot.com",
+    messagingSenderId: "681034223555",
+    appId: "1:681034223555:web:d8ef10b22cb5b381adb776",
+    measurementId: "G-WSRJTCWEFT"
+};
